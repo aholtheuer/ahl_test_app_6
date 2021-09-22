56247dd880eb6add501683f25b28ff1694e4f359
Andres Holtheuer <aholtheuer1@uc.cl>
2021-09-22 10:00:38 -0300

    Routes and refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
   def show
     #byebug
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -14,13 +16,12 @@ class ArticlesController < ApplicationController
 
   def edit
     #byebug
-    @article = Article.find(params[:id])
   end
 
   def create
     #render plain: params[:article]
-    @article = Article.new(params.require(:article).permit(:title, :description))
-    #render plain: @article.inspect
+    @article = Article.new(article_params)
+    # render plain: @article.inspect
     if @article.save
     #redirect_to article_path(@article)
       flash[:notice] = "Article was created successfully"
@@ -34,14 +35,31 @@ class ArticlesController < ApplicationController
 
   def update
     #byebug
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
     else
       render 'edit'
     end
 
+  end
+
+  def destroy 
+    @article.destroy
+    #para acordarte de los path ocupas el comando en la terminal
+    #rails routes --expanded y ves el prefijo de la ruta que quieres llegar
+    redirect_to articles_path
+
+  end
+
+  private 
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
-  resources :articles, only: [:show, :index, :create, :new, :edit, :update]
+  resources :articles #, only: [:show, :index, :create, :new, :edit, :update, :destroy]
 
 end
