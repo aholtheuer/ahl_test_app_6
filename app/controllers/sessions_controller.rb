fb8532487fb54c72598b39c264e1588d2dfa3996
Andres Holtheuer <aholtheuer1@uc.cl>
2021-09-24 12:27:32 -0300

    Add Sessions (Log In/Out) + some other little things

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome #user.username !"
+      redirect_to user_path(user.id)
+    else
+      flash.now[:alert] = "There was something wrong with your login"
+      render "new" 
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Succesfully logout"
+    redirect_to root_path
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,35 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
-    @articles = @user.articles
+    #@user = User.find(params[:id])
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def create
     @user = User.new(user_params)
     if @user.save 
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the blog #{@user.username} chupapija!"
-      redirect_to articles_path
+      redirect_to @user
     else
       render "new"
     end
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have edited your user data!"
       redirect_to articles_path
@@ -40,5 +42,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
