a7f0e13ffee2ed6b774f8fe5f7f4b5a76892ab3c
Andres Holtheuer <aholtheuer1@uc.cl>
2021-09-25 19:33:42 -0300

    Integration Test Cateogry + Adding new-create-show actions to Category Controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,33 @@
 class CategoriesController < ApplicationController
 
   def new 
+    @category = Category.new
 
   end
 
   def show
-
+    @category = Category.find(params[:id])
   end
 
   def index
     
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Categroy was succesfully created"
+      redirect_to category_path(@category)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  
 end
