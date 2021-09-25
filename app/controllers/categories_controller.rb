976b899c6a55eefc10b78d54052c74d9950d5eed
Andres Holtheuer <aholtheuer1@uc.cl>
2021-09-25 20:56:02 -0300

    Integretion test - Adding Admin user logged in to create category+

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
   def new 
     @category = Category.new
@@ -10,13 +11,13 @@ class CategoriesController < ApplicationController
   end
 
   def index
-    
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def create
     @category = Category.new(category_params)
     if @category.save
-      flash[:notice] = "Categroy was succesfully created"
+      flash[:notice] = "Category was succesfully created"
       redirect_to category_path(@category)
     else
       render 'new'
@@ -29,5 +30,11 @@ class CategoriesController < ApplicationController
     params.require(:category).permit(:name)
   end
 
+  def require_admin
+    if not (logeed_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
+  end
   
 end
--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin_user = User.create(username: 'johndoe', email: "johndoe@example.com",
+                              password: 'password', admin: true)
+    sign_in_as(@admin_user)
+      
+  end
+
   test "get new category form and create category" do
+
     get "/categories/new"
     assert_response :success
     assert_difference 'Category.count', 1 do
@@ -12,4 +20,17 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match "Travel", response.body
   end
+
+  test "get new category form and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: " " } }
+    end
+    assert_match "errors", response.body
+    # it is better to use assert_select 'html code especific for alert'
+    # becuase if you change the error message, you the test will not pass
+  end
+
+
 end
